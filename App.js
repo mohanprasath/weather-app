dc7cfb60cc5601a1de42cc2e730c0facd708b5ee
Mohan Prasath Chinnasamy <mohanprasath@users.noreply.github.com>
2020-08-10 19:21:34 +0300

    CYCLE 2 - PARTIALLY DONE

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const axios = require("axios");
 
 export default function App() {
+  const [cityName, setCityName] = useState("helsinki");
+  const API_URL = "http://api.openweathermap.org/data/2.5/weather?";
+  const api_key = "";
+  const [temperature, setTemperature] = useState("");
+  const [unitSystem, setUnitSystem] = useState("metric");
+
+  useEffect(() => {
+    load();
+    return () => {};
+  }, []);
+
+  function load() {
+    axios
+      .get(`${API_URL}q=${cityName}&units=${unitSystem}&appid=${api_key}`)
+      .then(function (response) {
+        // handle success
+        const jsonData = response.data;
+        console.log(jsonData, jsonData.main.temp);
+        setTemperature(jsonData.main.temp);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello User</Text>
+      <Text>
+        Hello User, the temperature is {temperature}º
+        {unitSystem === "metric" ? "C" : "F"}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +48,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- Plan.md
+++ Plan.md
@@ -6,9 +6,9 @@
 2. Set up workspace and required node and expo FS
 3. App should contain a text view of "Hello User"
 4. Add the weather app secret keys to the locale
-5. Planned duration - 20 Min
+5. Planned duration - 30 Min
 
-### Notes
+### Notes - cycle 1
 
 - .gitignore
 - npm install expo-cli --global
@@ -16,3 +16,26 @@
 - choosen blank template
 - copy the api secret to local env file
 - npm start
+
+### Report - cycle 1
+
+- ALL DONE
+
+## Cycle 2
+
+1. Make an api call and display the temperature with unit info on the app
+2. Choose by default the city to be Helsinki
+3. Log other info received from the api response to the console
+4. Planned duration - 45 min
+
+### Notes - cycle 2
+
+- axios vs. fetch - axios is cleaner to use
+- npm install axios
+- make a GET request with cityName and API_KEY
+- keeping secret key in env and importing it using the dotenv lib failed - BUG
+
+### Report - cycle 2
+
+- env key method has to be figured out
+- PARTIALLY DONE
